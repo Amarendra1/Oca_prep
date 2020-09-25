ab890f982d45e379cd0902377a0e420a97c30c27
Amarendra1 <61135017+Amarendra1@users.noreply.github.com>
2020-09-25 18:43:08 +0530

    Update Prog10.java

--- Oca/oca-preparation/src/Prog10.java
+++ Oca/oca-preparation/src/Prog10.java
@@ -1,13 +1,7 @@
-enum Animals{
-		DOG("woof"),CAT("MEOW"),FISH("burble");
-		String sound;
-		Animals(String s) { sound=s;}
-	}
-	public class Prog10 {
-		static Animals a;
-		public static void main(String[] args) {
-		System.out.println(a.DOG.sound + " " +a.FISH.sound);
-	}
-	}
+Select all valid methods of String class.
+A. trim()
+B. intern()
+C. toLower()
+D. split()
 
-//woof burble
+//Correct Answer: A, B
