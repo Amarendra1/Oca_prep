cb275dbbe2c2bd09a19fd3b3ae32505dd8d545d8
Amarendra1 <61135017+Amarendra1@users.noreply.github.com>
2020-09-25 18:59:37 +0530

    Update Prog7.java

--- Oca/oca-preparation/src/Prog7.java
+++ Oca/oca-preparation/src/Prog7.java
@@ -1,29 +1,12 @@
-import java.util.*;
-public class Prog7 {
-    public static void main(String args[]) {
-        // initialize ArrayList  
-        ArrayList<Integer> al = new ArrayList<Integer>();
-        // add elements to ArrayList object
-        al.add(3);
-        al.add(17);
-        al.add(6);
-        al.add(9);
-        al.add(7);
-        al.remove(2);
-         
-            for (Integer num : al) {         
-            System.out.println(num);   
-        }
-        if (al.contains(7)) {
-            System.out.println("7 was found in the list");
-        } else {
-            System.out.println("7 was not found in the list");
-        }
-    }
-}
+What will be the output of below statements?
 
-//3
-17
-9
-7
-7 was found in the list
+String s1 = "abc";
+String s2 = new String("abc");
+System.out.print(s1==s2);
+System.out.println(s1==s2.intern());
+A. falsetrue
+B. falsefalse
+C. truetrue
+D. truefalse
+
+//Correct Answer: A
